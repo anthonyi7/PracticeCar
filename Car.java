785867cea7197c5e2524caf54ad00695b5f4bd02
Anthony Irwin <anthonyirwin77@gmail.com>
2020-11-18 12:04:02 -0700

    11/18/20

---
+++ Car.java
@@ -0,0 +1,56 @@
+public class Car
+{
+	static void run()
+	{
+		System.out.println("hello");
+		
+	}
+}
+
+interface Vehicle
+{
+	public void function();
+		
+}
+
+class CarPart implements Vehicle
+{
+	static int condition = 100;
+	
+	static void status()
+	{
+		System.out.println("The condition of this vehicle is: " + condition);
+	}
+	
+	public void function() 
+	{
+		
+	}
+	
+}
+
+class Engine extends CarPart
+{
+	static String running = "Running";
+	static int temp= 300;
+	public void function()
+	{
+		System.out.println("The engine is " + running + " at " + temp + "degrees Fahrenheit");
+	}
+}
+
+class fuelTank extends CarPart
+{
+	int capacity;
+	int level;
+}
+
+class wheels extends CarPart
+{
+	int tread;
+}
+
+class windows extends CarPart
+{
+	boolean downOrUp;
+}
---
+++ Simulator.java
@@ -0,0 +1,8 @@
+public class Simulator
+{
+	public static void main(String[] args)
+	{
+		Car instanceOfCar = new Car();
+		instanceOfCar.run();
+	}
+}
